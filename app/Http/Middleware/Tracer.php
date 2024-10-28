1cd532c0cb16408cd2c34374f73665c3ed0a4ca3
GuuTAR <guitar20122012@gmail.com>
2024-10-29 00:06:14 +0700

    Refactor Tracing file

--- app/Http/Middleware/Tracer.php
+++ app/Http/Middleware/Tracer.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 
 use OpenTelemetry\Contrib\Otlp\SpanExporter;
@@ -20,6 +21,21 @@ use OpenTelemetry\API\Trace\SpanInterface;
 
 class Tracer
 {
+    private string $appname; // Service Name
+    private string $dbtype; // Database type such as mysql, sql_srv, etcs.
+    private string $dbname; // Database Name
+    private string $otlpEndpoint; // OpenTelemetry Endpoint
+    private string $otlpscope; // OpenTelemetry Scope Name
+
+    public function __construct()
+    {
+        $this->appname = env('APP_NAME', 'Unknown');
+        $this->dbtype = env('DB_CONNECTION', 'Unknown');
+        $this->dbname = env('DB_DATABASE', 'Unknown');
+        $this->otlpEndpoint = env('IBM_INSTANA_OTLP_ENDPOINT', 'http://localhost:4318/v1/traces');
+        $this->otlpscope = env('IBM_INSTANA_OTLP_SCOPE', 'Unknown');
+    }
+
     /**
      * Handle an incoming request.
      *
@@ -29,26 +45,11 @@ class Tracer
      */
     public function handle(Request $request, Closure $next)
     {
-        // Create a new span for the incoming request
-        $path = $request->fullUrl();
-        $method = $request->getMethod();
-        $body = $request->input();
-        $query = $request->query();
-
-        // Start the span for tracing the request
-        $appname = env('APP_NAME', 'Unknown');
-
-        $parent = $this->getTracerProvider($appname)->spanBuilder("HTTP $method $path")->startSpan();
-        $parent->setAttribute('http.method', $method);
-        $parent->setAttribute('http.url', $path);
-        $parent->setAttribute('http.body', $body);
-        $parent->setAttribute('http.query', $query);
-
-        // Process the request
         try {
             // Enable query logging
             DB::enableQueryLog();
 
+            // Process the request
             // Pass the request to the next middleware/controller
             $response = $next($request);
 
@@ -56,10 +57,18 @@ class Tracer
             $queries = DB::getQueryLog();
             DB::flushQueryLog();
 
-            $this->getSQLStatement($parent, $queries);
-
-            // Set HTTP status code in the trace
+            // Retrieve Tracing
+            $parent = $this->recordHttpMetrics($request);
             $parent->setAttribute('http.status_code', $response->getStatusCode());
+
+            // Get controller and method information
+            $this->recordControllerInfo($parent);
+
+            // Retrieve SQL Statement
+            $this->recordSQLStatement($parent, $queries);
+
+            // Stop the span for tracing the request
+            $parent->end();
         } catch (\Exception $e) {
             // Capture any exception that occurs during the request
             $parent->recordException($e);
@@ -67,50 +76,77 @@ class Tracer
 
             throw $e; // Continue propagating the exception
         }
-
-        $parent->end();
         return $response;
     }
 
-    private function getSQLStatement(SpanInterface $parent, array $queries)
+    private function recordControllerInfo(SpanInterface $span): void
+    {
+        $route = Route::current();
+
+        if ($route) {
+            $action = $route->getAction();
+            $controller = $action['controller'] ?? 'Unknown';
+
+            // Get controller and method separately
+            if (is_string($controller) & $controller != 'Unknown') {
+                [$controllerName, $methodName] = explode('@', $controller);
+                $span->setAttribute('controller.name', class_basename($controllerName));
+                $span->setAttribute('controller.method', $methodName);
+            }
+        }
+    }
+
+    private function recordHttpMetrics(Request $request)
+    {
+        // Start the span for tracing the request
+        $span = $this->getTracerProvider($this->appname)
+            ->spanBuilder("HTTP {$request->getMethod()} {$request->fullUrl()}")
+            ->startSpan();
+
+        // Retrieve http metrics
+        $span->setAttribute('http.method', $request->getMethod());
+        $span->setAttribute('http.url', $request->fullUrl());
+        $span->setAttribute('http.body', $request->input());
+        $span->setAttribute('http.query', $request->query());
+
+        return $span;
+    }
+
+    private function recordSQLStatement(SpanInterface $parent, array $queries)
     {
         // Retrieve the current parent span
         $scope = $parent->activate();
+        $tracer = $this->getTracerProvider($this->dbtype);
 
-        $dbtype = env('DB_CONNECTION', 'Unknown');
-        $dbname = env('DB_DATABASE', 'Unknown');
+        foreach ($queries as $query) {
+            // Start a new span for each SQL query
+            $querySpan = $tracer->spanBuilder("SQL Statement")->startSpan();
 
-        $tracer = $this->getTracerProvider($dbtype);
+            // Retrieve database metrics
+            $querySpan->setAttribute('db.statement', $query['query']);
+            $querySpan->setAttribute('db.params', json_encode($query['bindings']));
+            $querySpan->setAttribute('db.type', $this->dbtype);
+            $querySpan->setAttribute('db.name', $this->dbname);
+            $querySpan->setAttribute('db.execution_time_ms', $query['time']);
 
-        try {
-            foreach ($queries as $query) {
-                // Start a new span for each SQL query
-                $querySpan = $tracer->spanBuilder("SQL Statement")->startSpan();
-                $querySpan->setAttribute('db.statement', $query['query']);
-                $querySpan->setAttribute('db.params', json_encode($query['bindings']));
-                $querySpan->setAttribute('db.type', $dbtype);
-                $querySpan->setAttribute('db.name', $dbname);
-                $querySpan->setAttribute('db.execution_time_ms', $query['time']);
-                // End the query span
-                $querySpan->end();
-            }
-        } finally {
-            $scope->detach();
+            // End the query span
+            $querySpan->end();
         }
+        $scope->detach();
     }
 
     private function getTracerProvider(string $serviceName)
     {
+        // Initialize Resource Infomation
         $resource = ResourceInfoFactory::emptyResource()->merge(ResourceInfo::create(Attributes::create([
             ResourceAttributes::SERVICE_NAME => $serviceName,
         ])));
 
-        $transport = (new OtlpHttpTransportFactory())->create('http://localhost:4318/v1/traces', 'application/json');
+        // Get Exporter 
+        $transport = (new OtlpHttpTransportFactory())->create($this->otlpEndpoint, 'application/json');
+        $spanExporter = new SpanExporter($transport);
 
-        $spanExporter = new SpanExporter(
-            $transport
-        );
-
+        // Create Tracer Provider 
         $tracerProvider = TracerProvider::builder()
             ->addSpanProcessor(
                 new SimpleSpanProcessor($spanExporter)
@@ -119,8 +155,6 @@ class Tracer
             ->setSampler(new ParentBased(new AlwaysOnSampler()))
             ->build();
 
-        return $tracerProvider->getTracer(
-            'WEBINTRA-ZONE'
-        );
+        return $tracerProvider->getTracer($this->otlpscope);
     }
 }
