430b9f6a0a8455a2be4c514becfd188c611febba
GuuTAR <guitar20122012@gmail.com>
2024-11-05 19:14:04 +0700

    Optimize tracer performance

--- app/Http/Middleware/Tracer.php
+++ app/Http/Middleware/Tracer.php
@@ -7,18 +7,18 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 
-use OpenTelemetry\Contrib\Otlp\SpanExporter;
 use OpenTelemetry\SDK\Common\Attribute\Attributes;
 use OpenTelemetry\SDK\Resource\ResourceInfo;
 use OpenTelemetry\SDK\Resource\ResourceInfoFactory;
-use OpenTelemetry\SDK\Trace\Sampler\AlwaysOnSampler;
-use OpenTelemetry\SDK\Trace\Sampler\ParentBased;
-use OpenTelemetry\SDK\Trace\SpanProcessor\SimpleSpanProcessor;
 use OpenTelemetry\SDK\Trace\TracerProvider;
-use OpenTelemetry\SemConv\ResourceAttributes;
+use OpenTelemetry\SDK\Trace\SpanProcessor\BatchSpanProcessorBuilder;
+use OpenTelemetry\SDK\Trace\SpanProcessor\SimpleSpanProcessor;
 use OpenTelemetry\Contrib\Otlp\OtlpHttpTransportFactory;
+use OpenTelemetry\Contrib\Otlp\SpanExporter;
 use OpenTelemetry\API\Trace\SpanInterface;
 use OpenTelemetry\API\Trace\SpanKind;
+use OpenTelemetry\API\Trace\TracerInterface;
+use OpenTelemetry\SemConv\ResourceAttributes;
 
 class Tracer
 {
@@ -30,7 +30,9 @@ class Tracer
     private string $otlpscope; // OpenTelemetry Scope Name
     private string $otlpContentType; // OpenTelemetry Content Type
     private string $otelEnabled; // OpenTelemetry Enabled status
-    private string $otelSQLSperateTraceEnabled;
+
+    private TracerProvider $tracerProvider;
+    private TracerInterface $tracer;
 
     public function __construct()
     {
@@ -42,7 +44,8 @@ class Tracer
         $this->otlpscope = env('IBM_INSTANA_OTLP_SCOPE', 'Unknown');
         $this->otlpContentType = env('IBM_INSTANA_OTLP_CONTENT_TYPE', 'application/json');
         $this->otelEnabled = env('IBM_INSTANA_OTEL_ENABLED', false);
-        $this->otelSQLSperateTraceEnabled = false;
+
+        $this->tracer = $this->getTracerProvider($this->appname);
     }
 
     /**
@@ -54,45 +57,32 @@ class Tracer
      */
     public function handle(Request $request, Closure $next)
     {
+        $starttime = microtime(true);
         try {
             if (!$this->otelEnabled) {
                 $response = $next($request);
                 return $response;
             }
 
-            if ($this->otelSQLSperateTraceEnabled) {
-                $this->enableSeperateSQLTrace();
-            }
-
-            // Enable query logging
-            DB::enableQueryLog();
-
             // Retrieve Tracing
             $parent = $this->recordHttpMetrics($request);
 
+            // Enable query logging
+            $this->recordSQLStatement($parent);
+
+            $requestStarttime = microtime(true);
 
             // Process the request
             // Pass the request to the next middleware/controller
             $response = $next($request);
 
-            // Retrieve executed queries after the request
-            $queries = DB::getQueryLog();
-            DB::flushQueryLog();
+            $requestEndtime = microtime(true);
+            $requestTime = $requestEndtime - $requestStarttime;
+
             $parent->setAttribute('http.status_code', $response->getStatusCode());
 
             // Get controller and method information
             $this->recordControllerInfo($parent);
-
-            // Retrieve SQL Statement
-            $scope = $parent->activate();
-
-            foreach ($queries as $query) {
-                $this->recordSQLStatement($query);
-            }
-            $scope->detach();
-
-            // Stop the span for tracing the request
-            $parent->end();
         } catch (\Exception $e) {
             // Capture any exception that occurs during the request
             $parent->recordException($e);
@@ -100,6 +90,19 @@ class Tracer
 
             throw $e; // Continue propagating the exception
         }
+        $endtime = microtime(true);
+        $totalTime = $endtime - $starttime;
+
+        $traceTime = $totalTime - $requestTime;
+        $traceTimePercent = ($traceTime * 100) / $totalTime;
+
+        // Stop the span for tracing the request
+        $parent->setAttribute('http.total_time', $totalTime);
+        $parent->setAttribute('tracing.time_s', $traceTime);
+        $parent->setAttribute('tracing.time_percent', $traceTimePercent);
+        $parent->end();
+
+        $this->tracerProvider->shutdown();
         return $response;
     }
 
@@ -120,11 +123,10 @@ class Tracer
         }
     }
 
-    private function recordHttpMetrics(Request $request)
+    private function recordHttpMetrics(Request $request): SpanInterface
     {
         // Start the span for tracing the request
-        $span = $this->getTracerProvider($this->appname)
-            ->spanBuilder("{$request->getMethod()} {$request->getRequestUri()}")
+        $span = $this->tracer->spanBuilder("{$request->getMethod()} {$request->getRequestUri()}")
             ->setSpanKind(SpanKind::KIND_SERVER)
             ->startSpan();
 
@@ -140,31 +142,44 @@ class Tracer
         return $span;
     }
 
-    private function getDbOperation($dbStatement)
+    private function getDbOperation($dbStatement): string
     {
         if (preg_match('/^\s*(SELECT|INSERT|UPDATE|DELETE|CREATE|DROP|ALTER|TRUNCATE|REPLACE)\b/i', $dbStatement, $matches)) {
             return strtoupper($matches[1]);
         }
         return 'UNKNOWN';
     }
-    private function recordSQLStatement($query)
-    {
-        $tracer = $this->getTracerProvider($this->dbtype);
-        $querySpan = $tracer->spanBuilder($query['query'])->startSpan();
 
-        // Retrieve database metrics
-        $querySpan->setAttribute('db.statement', $query['query']);
-        $querySpan->setAttribute('db.operation', $this->getDbOperation($query['query']));
-        $querySpan->setAttribute('db.params', json_encode($query['bindings']));
-        $querySpan->setAttribute('db.system', $this->dbtype);
-        $querySpan->setAttribute('db.name', $this->dbname);
-        $querySpan->setAttribute('db.execution_time_ms', $query['time']);
-        $querySpan->setAttribute('service.name', $this->dbServiceName);
-        // End the query span
-        $querySpan->end();
+    private function recordSQLStatement(SpanInterface $parent): SpanInterface
+    {
+        DB::listen(function ($query) use ($parent) {
+            // Retrieve SQL Statement
+            $scope = $parent->activate();
+
+            // Retrieve query span
+            $querySpan = $this->tracer->spanBuilder($query->sql)->startSpan();
+
+            // Retrieve database metrics
+            $querySpan->setAttribute('db.statement', $query->sql);
+            $querySpan->setAttribute('db.operation', $this->getDbOperation($query->sql));
+            $querySpan->setAttribute('db.params', json_encode($query->bindings));
+            $querySpan->setAttribute('db.system', $this->dbtype);
+            $querySpan->setAttribute('db.name', $this->dbname);
+            $querySpan->setAttribute('db.execution_time_ms', $query->time);
+            $querySpan->setAttribute('service.name', $this->dbServiceName);
+
+            // End the query span
+            $querySpan->end();
+
+            $scope->detach();
+        });
+
+        DB::flushQueryLog();
+
+        return $parent;
     }
 
-    private function getTracerProvider(string $serviceName)
+    private function getTracerProvider(string $serviceName): TracerInterface
     {
         // Initialize Resource Infomation
         $resource = ResourceInfoFactory::emptyResource()->merge(ResourceInfo::create(Attributes::create([
@@ -178,35 +193,14 @@ class Tracer
         // Create Tracer Provider 
         $tracerProvider = TracerProvider::builder()
             ->addSpanProcessor(
-                new SimpleSpanProcessor($spanExporter)
+                // new SimpleSpanProcessor($spanExporter)
+                (new BatchSpanProcessorBuilder($spanExporter))->build()
             )
             ->setResource($resource)
-            ->setSampler(new ParentBased(new AlwaysOnSampler()))
             ->build();
 
+        $this->tracerProvider = $tracerProvider;
+
         return $tracerProvider->getTracer($this->otlpscope);
-    }
-
-    public function enableSeperateSQLTrace()
-    {
-        DB::listen(function ($query) {
-            // Retrieve query span
-            $querySpan = $this->getTracerProvider($this->dbServiceName)
-                ->spanBuilder($query->sql)
-                ->setSpanKind(SpanKind::KIND_SERVER)
-                ->startSpan();
-
-            // Retrieve database metrics
-            $querySpan->setAttribute('db.statement', $query->sql);
-            $querySpan->setAttribute('db.operation', $this->getDbOperation($query->sql));
-            $querySpan->setAttribute('db.params', json_encode($query->bindings));
-            $querySpan->setAttribute('db.system', $this->dbtype);
-            $querySpan->setAttribute('db.name', $this->dbname);
-            $querySpan->setAttribute('db.execution_time_ms', $query->time);
-            $querySpan->setAttribute('service.name', $this->dbServiceName);
-
-            // End the query span
-            $querySpan->end();
-        });
     }
 }
